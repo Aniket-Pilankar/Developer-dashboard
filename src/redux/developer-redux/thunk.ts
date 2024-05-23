99ee0102d631a8321cd727d31dda85cb28bf9dbe
Aniket Pilankar <aniketpilankar1998@gmail.com>
2024-05-23 11:09:32 +0530

    add json-server deployed url

--- src/redux/developer-redux/thunk.ts
+++ src/redux/developer-redux/thunk.ts
@@ -4,7 +4,9 @@ import axios from "axios";
 export const fetchDeveloperActivity = createAsyncThunk(
   "developer-activity/fetch/all",
   async () => {
-    const response = await axios.get(`http://localhost:8000/data`);
+    const response = await axios.get(
+      `https://developer-dashboard-json.vercel.app/data`
+    );
     return response.data;
   }
 );
